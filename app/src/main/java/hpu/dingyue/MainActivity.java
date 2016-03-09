a91afb7eba6f03fa940a47b3ceba64a2036fc62a
hpu2010 <donghan2010@126.com>
2016-03-09 11:25:44 +0800

    123456

--- app/src/main/java/hpu/dingyue/MainActivity.java
+++ app/src/main/java/hpu/dingyue/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,6 +25,9 @@ import java.util.List;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
     private FragmentManager supportFragmentManager;
@@ -32,81 +36,31 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
-    private Observer observer = new Observer() {// 创建观察者
-        @Override
-        public void onCompleted() {
-
-        }
-
-        @Override
-        public void onError(Throwable e) {
-
-        }
-
-        @Override
-        public void onNext(Object o) {
-
-        }
-    };
-
-    private Subscriber subscriber = new Subscriber() {// 创建观察者，与Observer类似
-        @Override
-        public void onStart() {// 它总是在 subscribe 所发生的线程被调用，而不能指定线程。
-            super.onStart();
-        }
-
-        @Override
-        public void onCompleted() {
-
-        }
-
-        @Override
-        public void onError(Throwable e) {
-
-        }
-
-        @Override
-        public void onNext(Object o) {
-
-        }
-    };
-
-    private Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
-        @Override
-        public void call(Subscriber<? super String> subscriber) {
-
-        }
-    });
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         supportFragmentManager = getSupportFragmentManager();
         myAdapter = new MyAdapter(supportFragmentManager);
 
         Button btn = (Button) findViewById(R.id.btn);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
         btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn:
-                new Thread(new Runnable() {
+                /*new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        if (isInMainThread()) {
-                            Log.i("isInMainThread:", "我在主线程");
-                        } else {
-                            Log.i("isInMainThread:", "我在子线程");
-                        }
-                        long id = Thread.currentThread().getId();
-                        String name = Thread.currentThread().getName();
-                        Log.i("id = " + id, "; name = " + name);
+
                         try {
                             Document document = Jsoup.connect("http://www.androidweekly.cn/").get();
                             Elements links = document.select("h2 a");// 获取h2元素下的所有a元素
@@ -148,8 +102,103 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                             e.printStackTrace();
                         }
                     }
-                }).start();
+                }).start();*/
 
+
+                Observable.create(new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        try {
+                            Document document = Jsoup.connect("http://www.androidweekly.cn/").get();
+                            Elements links = document.select("h2 a");// 获取h2元素下的所有a元素
+                            List<String> titleList = new ArrayList<String>();
+                            List<String> linkList = new ArrayList<String>();
+                            for(Element link : links) {
+                                linkList.add(link.attr("abs:href"));
+                                titleList.add(link.text());
+//                                System.out.println(link.attr("abs:href") + link.text());
+                            }
+
+                            Elements contents = document.select("section p");
+                            List<String> contentList = new ArrayList<String>();
+                            for(Element content : contents) {
+                                contentList.add(content.text());
+//                                System.out.println(content.text());
+                            }
+
+                            Elements times = document.getElementsByTag("time");
+                            List<String> timeList = new ArrayList<String>();
+                            for (Element time : times) {
+                                timeList.add(time.text());
+//                                System.out.println(time.text());
+                            }
+                            mList.add(titleList);
+                            mList.add(linkList);
+                            mList.add(contentList);
+                            mList.add(timeList);
+
+                            subscriber.onCompleted();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Observer() {
+                            @Override
+                            public void onCompleted() {
+                                viewPager.setAdapter(myAdapter);
+                                tabLayout.setupWithViewPager(viewPager);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                            }
+
+                            @Override
+                            public void onNext(Object o) {
+
+                            }
+                        });
+                break;
+            case R.id.btn2:
+                Observable.create(new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        subscriber.onNext("hellow");
+                        subscriber.onNext("rx");
+                        subscriber.onNext("android");
+                        subscriber.onCompleted();
+                    }
+                })
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<String>() {
+                            @Override
+                            public void onCompleted() {
+                                Log.d("complete", "结束");
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+
+                            @Override
+                            public void onNext(String s) {
+                                Log.d("tag", s);
+                            }
+                        });
+                break;
+            case R.id.btn3:
+                try {
+                    int i = 2/0;
+                    Log.e("接着：", "------------------");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Log.e("出来了：", "------------------");
                 break;
         }
     }
