00f09628e2a5c29c36594bd0f745496ff84c0001
hpu2010 <donghan2010@126.com>
2016-10-18 10:02:39 +0800

    change: 测试推送test3分支

--- app/src/main/java/hpu/dingyue/LoginActivity.java
+++ app/src/main/java/hpu/dingyue/LoginActivity.java
@@ -270,8 +270,6 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
                 Log.e("数据库路径", path);
             }
         }).start();
-        Log.e("数据库路径","我是test2分支");
+        Log.e("数据库路径","我是test3分支");
     }
-
-
 }
